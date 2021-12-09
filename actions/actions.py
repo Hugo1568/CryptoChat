cc6f93e39d0c115e1f10cba8251d348f2b23038d
Hugo Cano <43715510+Hugo1568@users.noreply.github.com>
2021-12-08 21:28:51 -0700

    Chatbot finished with professsor's requierements

--- actions/actions.py
+++ actions/actions.py
@@ -14,7 +14,8 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted, EventType
 import requests
 import re
-import secrets
+
+CMC_API_KEY = "API_KEY"
 
 class ActionCryptoPrice(Action):
 
@@ -32,7 +33,7 @@ class ActionCryptoPrice(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         headers = {
-                'X-CMC_PRO_API_KEY': secrets.API_KEY,
+                'X-CMC_PRO_API_KEY': CMC_API_KEY,
                 'Accepts': 'application/json'
         }
 
@@ -48,9 +49,13 @@ class ActionCryptoPrice(Action):
         coins =  json['data']
 
         CryptoName = str(tracker.get_slot("crypto"))
+        if CryptoName is None:
+            CryptoName = str(tracker.get_slot("crypto2"))
+        if re.match(r'\b[a-zA-Z]{3,6}\b', CryptoName):
+            CryptoName = CryptoName.upper()
 
 
-        display_text="La moneda no se encontró, o no está en el top 200 de criptomonedas, intenta utilizar su símbolo. ejemplo: Bitocin: BTC"
+        display_text="La moneda no se encontró, o no está en el top 200 de criptomonedas, intenta utilizar su símbolo. ejemplo: Bitcoin: BTC"
         if CryptoName is None:
             display_text="Te mamaste no hay token"
         if CryptoName is not None:
@@ -61,12 +66,6 @@ class ActionCryptoPrice(Action):
                     display_text = ("El precio del " + str(coin['slug']) + " es de: $" + str(round(coin['quote']['MXN']['price'],4) ) + " pesos mexicanos" + "\n")
                 if coin['symbol'] == CryptoName:
                     display_text = ("El precio del " + str(coin['slug']) + " es de: $" + str(round(coin['quote']['MXN']['price'],4) ) + " pesos mexicanos" + "\n")
-                else:
-                    if re.match(r'\b[a-zA-Z]{3,6}\b', CryptoName):
-                        CryptoName = CryptoName.upper()
-                        if coin['symbol'] == CryptoName:
-                            display_text = ("El precio del " + str(coin['slug']) + " es de: $" + str(round(coin['quote']['MXN']['price'],4) ) + " pesos mexicanos" + "\n")
-
             
 
         dispatcher.utter_message(text=display_text)
@@ -77,12 +76,12 @@ class ActionCryptoPrice(Action):
 class ActionCryptoCompare(Action):
 
     def name(self) -> Text:
-        return "action_crypto_price"
+        return "action_crypto_compare"
 
     @staticmethod
     def fetch_slots(tracker: Tracker):
         #Collects slots on crypto price
-        tracker.get_slot("crypto")
+        tracker.get_slot("crypto", "crypto2")
 
 
     def run(self, dispatcher: CollectingDispatcher,
@@ -90,7 +89,7 @@ class ActionCryptoCompare(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         headers = {
-                'X-CMC_PRO_API_KEY': secrets.API_KEY,
+                'X-CMC_PRO_API_KEY': CMC_API_KEY,
                 'Accepts': 'application/json'
         }
 
@@ -105,27 +104,38 @@ class ActionCryptoCompare(Action):
         json = requests.get(url, params = paramsMXN, headers = headers).json()
         coins =  json['data']
 
-        CryptoName = str(tracker.get_slot("crypto"))
+        CryptoName= str(tracker.get_slot("crypto"))
+        CryptoName2= str(tracker.get_slot("crypto2"))
+
+        if re.match(r'\b[a-zA-Z]{3,6}\b', CryptoName):
+                        CryptoName = CryptoName.upper()
+        if re.match(r'\b[a-zA-Z]{3,6}\b', CryptoName2):
+                        CryptoName2 = CryptoName2.upper()
 
 
         display_text="La moneda no se encontró, o no está en el top 200 de criptomonedas, intenta utilizar su símbolo. ejemplo: Bitocin: BTC"
-        if CryptoName is None:
-            display_text="Te mamaste no hay token"
-        if CryptoName is not None:
-            for coin in coins:
-                if coin['slug'] == CryptoName:
-                    display_text = ("El precio del " + str(coin['slug']) + " es de: $" + str(round(coin['quote']['MXN']['price'],4) ) + " pesos mexicanos" + "\n")
-                if coin['name'] == CryptoName:
-                    display_text = ("El precio del " + str(coin['slug']) + " es de: $" + str(round(coin['quote']['MXN']['price'],4) ) + " pesos mexicanos" + "\n")
-                if coin['symbol'] == CryptoName:
-                    display_text = ("El precio del " + str(coin['slug']) + " es de: $" + str(round(coin['quote']['MXN']['price'],4) ) + " pesos mexicanos" + "\n")
-                else:
-                    if re.match(r'\b[a-zA-Z]{3,6}\b', CryptoName):
-                        CryptoName = CryptoName.upper()
-                        if coin['symbol'] == CryptoName:
-                            display_text = ("El precio del " + str(coin['slug']) + " es de: $" + str(round(coin['quote']['MXN']['price'],4) ) + " pesos mexicanos" + "\n")
-
-            
+        for coin in coins:
+            if coin['symbol'] == CryptoName or coin['slug'] == CryptoName or coin['name'] == CryptoName:
+                VAR1 = round(coin['quote']['MXN']['price'],4)
+                CryptoName = coin['name']
+            if coin['slug'] == CryptoName:
+                VAR1 = round(coin['quote']['MXN']['price'],4)
+                CryptoName = coin['name']
+            if coin['name'] == CryptoName:
+                VAR1 = round(coin['quote']['MXN']['price'],4)
+                CryptoName = coin['name']
+            if coin['symbol'] == CryptoName2:
+                VAR2 = round(coin['quote']['MXN']['price'],4)
+                CryptoName2 = coin['name']
+            if coin['slug'] == CryptoName2:
+                VAR2 = round(coin['quote']['MXN']['price'],4)
+                CryptoName2 = coin['name']
+            if coin['name'] == CryptoName2:
+                VAR2 = round(coin['quote']['MXN']['price'],4)
+                CryptoName2 = coin['name']
+        if VAR1 and VAR2 is not None:
+            VAR3 = VAR1/VAR2
+            display_text= ("El " + CryptoName + " es aproximadamente " + str(format(VAR3,'.8f')) + " " +CryptoName2 +"s")
 
         dispatcher.utter_message(text=display_text)
 
